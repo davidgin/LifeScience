3517fe652bdd855b9b2b5f3f05b881d3d3c011ec
davidgin <davidginzburg@gmail.com>
2022-03-06 00:00:00 -0700

    Add OSM retry logic in FetchOSMLocationWithRetry

--- osm.go
+++ osm.go
@@ -3,10 +3,30 @@ package main
 import (
     "encoding/json"
     "fmt"
+    "log"
     "net/http"
+    "time"
 )
 
 var OSMClient = &http.Client{Timeout: 10 * time.Second}
+
+func FetchOSMLocationWithRetry(query string) (string, error) {
+    const maxRetries = 3
+    const retryDelay = 60 * time.Minute
+
+    for attempt := 0; attempt <= maxRetries; attempt++ {
+        location, err := FetchOSMLocation(query)
+        if err == nil {
+            return location, nil
+        }
+        log.Printf("OSM fetch attempt %d failed: %v", attempt+1, err)
+        if attempt < maxRetries {
+            log.Printf("Retrying in %d minutes...", int(retryDelay.Minutes()))
+            time.Sleep(retryDelay)
+        }
+    }
+    return "", fmt.Errorf("failed to fetch OSM location after %d retries", maxRetries)
+}
 
 func FetchOSMLocation(query string) (string, error) {
     url := fmt.Sprintf("https://nominatim.openstreetmap.org/search?q=%s&format=json&limit=1", query)

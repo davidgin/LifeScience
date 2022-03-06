d835043e7c93b0a7a60617389692d5be8c655aa7
davidgin <davidginzburg@gmail.com>
2022-03-06 00:00:00 -0700

    Add goroutine for async PostgreSQL insert in CreateEvent

--- handlers.go
+++ handlers.go
@@ -3,6 +3,7 @@ package main
 import (
     "context"
     "log"
+    "sync"
     "time"
     "github.com/gin-gonic/gin"
 )
@@ -15,17 +16,27 @@ func CreateEvent(c *gin.Context) {
     }
     event.Timestamp = time.Now()
 
-    result, err := DB.ExecContext(context.Background(),
-        "INSERT INTO events (title, description, timestamp) VALUES ($1, $2, $3) RETURNING id",
-        event.Title, event.Description, event.Timestamp)
-    if err != nil {
-        log.Println("PostgreSQL error:", err)
-        c.JSON(500, gin.H{"error": err.Error()})
-        return
-    }
-    var id int64
-    result.Scan(&id)
-    event.ID = id
+    var mu sync.Mutex
+    var wg sync.WaitGroup
+    wg.Add(1)
+
+    go func() {
+        defer wg.Done()
+        result, err := DB.ExecContext(context.Background(),
+            "INSERT INTO events (title, description, timestamp) VALUES ($1, $2, $3) RETURNING id",
+            event.Title, event.Description, event.Timestamp)
+        if err != nil {
+            log.Println("PostgreSQL error:", err)
+            return
+        }
+        var id int64
+        result.Scan(&id)
+        mu.Lock()
+        event.ID = id
+        mu.Unlock()
+    }()
+
+    wg.Wait()
     c.JSON(201, event)
 }
 

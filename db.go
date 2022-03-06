89cbe15b65e67dd00ab8011ece2e2596a2d9b6ce
davidgin <davidginzburg@gmail.com>
2022-03-06 00:00:00 -0700

    Switch to sqlx for PostgreSQL in db.go

--- db.go
+++ db.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "database/sql"
     "log"
+    "github.com/jmoiron/sqlx"
     _ "github.com/lib/pq"
 )
 
-var DB *sql.DB
+var DB *sqlx.DB
 
 func InitDB() {
     var err error
-    DB, err = sql.Open("postgres", "postgres://life_user:life_pass123@localhost:5432/life_science_db?sslmode=disable")
+    DB, err = sqlx.Open("postgres", "postgres://life_user:life_pass123@localhost:5432/life_science_db?sslmode=disable")
     if err != nil {
         log.Fatal("Failed to connect to PostgreSQL:", err)
     }

eee8b6f3330f429a3b6d8eddc1fdbd7837f9ee8d
davidgin <davidginzburg@gmail.com>
2022-03-06 00:00:00 -0700

    Add Event struct in models.go

--- models.go
+++ models.go
@@ -7,7 +7,4 @@ type Event struct {
     Title       string    `json:"title" db:"title"`
     Description string    `json:"description" db:"description"`
     Timestamp   time.Time `json:"timestamp" db:"timestamp"`
-    Processed   bool      `json:"processed" db:"processed"`
-    Location    string    `json:"location" db:"location"`
-    VideoURL    string    `json:"video_url" db:"video_url"`
 }
